f7a70f790c9089f3b3a64060ee59a9489e3b53ac
Piotrek <p.niezgoda@hotmail.com>
2019-04-10 19:12:20 +0200

    revert

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,6 +5,9 @@ require('../../index.html');
 require('../../informacje.html');
 require('../../prace.html');
 require('../scss/style.scss');
+require('../scss/style_home.scss');
+require('../scss/style_info.scss');
+require('../scss/style_works.scss');
 
 
 settingsBox();
