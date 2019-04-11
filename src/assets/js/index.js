90b2cb85df175269e22eafb1a61b7971650cb266
Piotrek <p.niezgoda@hotmail.com>
2019-04-11 12:32:15 +0200

    change navigation

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,9 +5,7 @@ require('../../index.html');
 require('../../informacje.html');
 require('../../prace.html');
 require('../scss/style.scss');
-require('../scss/style_home.scss');
-require('../scss/style_info.scss');
-require('../scss/style_works.scss');
+
 
 
 settingsBox();
